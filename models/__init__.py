966f2991c92245735e4bb61f6e175a79ddb22ec1
dave motari <davemotari22@gmail.com>
2022-09-09 13:47:58 +0300

    basemodel correction

--- models/__init__.py
+++ models/__init__.py
@@ -1,8 +1,9 @@
-#!/usr/bin/python3
-"""
+"""#!/usr/bin/python3
+
 Creates a unique FileStorage instance for your applicatio
-"""
+
 from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
+"""
