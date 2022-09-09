d673424a1bd099f85953958c3377b981c292cdbe
dave motari <davemotari22@gmail.com>
2022-09-09 13:26:31 +0300

    Basemodel correction

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,7 @@ defines all common attributes/methods for other classes
 """
 import uuid
 from datetime import datetime
-from models import storage
+# from models import storage
 
 
 class BaseModel():
@@ -18,17 +18,21 @@ class BaseModel():
         arguments: *args - pointer to list of commandline arguments
         **kwargs - list to key, value arguements.
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-        if kwargs is not None or kwargs != {}:
+        time_format = "%Y-%m-%dT%H:%M:%S.%f"
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
-                    value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != '__class__':
-                    setattr(self, key, value)
+                if key == "created_at":
+                    self.__dict__["created_at"] = datetime.strptime(value, time_format)
+                elif key == "updated_at":
+                    self.__dict__["updated_at"] = datetime.strptime(value, time_format)
+                else:
+                    self.__dict__[key] = kwargs[key]
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+#            storage.new(self)
 
-    # models.storage.new(self)
     def __str__(self):
         """
         Returns: instance of BaseModel
@@ -42,7 +46,7 @@ class BaseModel():
         updated_at with the current datetime
         """
         self.updated_at = datetime.utcnow()
-        storage.save()
+#        storage.save()
 
     # models.storage.save()
     def to_dict(self):
