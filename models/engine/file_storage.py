edf6efa118ceaa25062f52e9357464de855455c4
Lesley-w19 <wanjiku.kamash19@gmail.com>
2022-10-31 22:04:29 +0300

    5. Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -23,29 +23,35 @@ class FileStorage:
         self.__file_path = "file.json"
         self.__objects = {}
 
-    def all(self):
+    def all(self, clas=None):
         """
         returns the dictionary __objects
         """
+        if clas is not None:
+            new_dict = {}
+            for key, value in self.__objects.items():
+                if clas == value.__class__ or clas == value.__class__.__name__ : new_dict[key] = value
+                return new_dict
         return self.__objects
 
     def new(self, obj):
         """
         sets in __objects the obj with key <obj class name>.id
         """
-        ke_y = "{}.{}".format(obj.__class__.__name__, obj.id)
-        self.__objects[ke_y] = obj
+        if obj is not None:
+            key = "{}.{}".format(obj.__class__.__name__, obj.id)
+            self.__objects[key] = obj
 
     def save(self):
         """save the objects dictionary into file
         make serializable dict objects
         """
         with open(self.__file_path, "w") as f:
-            o_dict = {}
-            o_dict.update(self.__objects)
-            for k, v in o_dict.items():
-                o_dict[k] = v.to_dict()
-            json.dump(o_dict, f)
+            json_object = {}
+            json_object.update(self.__objects)
+            for key, value in json_object.items():
+                json_object[key] = value.to_dict()
+            json.dump(json_object, f)
 
     def reload(self):
         """reload objects from file"""
